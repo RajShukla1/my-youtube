33b072b3ee97c7d5d5cf68593ab033553d261191
sprinix <rajshukla140@gmail.com>
2023-11-28 16:37:39 +0530

    autoplay video

--- src/components/Head.js
+++ src/components/Head.js
@@ -5,18 +5,24 @@ import { YOUTUBE_SEARCH_API } from "../utils/constants";
 
 const Head = () => {
   const [searchQuery, setSearchQuery] = useState("");
+  const [show,setShow] = useState(false);
 
   const dispatch = useDispatch();
   const toggleMenuHandler = () => {
     dispatch(toggleMenu());
   };
   const getSearchSuggestions = async ()=>{
-    const data = await fetch(YOUTUBE_SEARCH_API + searchQuery);
-    const json = await data.json();
+    const data = (await fetch(YOUTUBE_SEARCH_API + searchQuery)).catch((e)=>searchQuery);
+    const json = await data.json().catch((e)=>searchQuery);
     console.log(json);
   }
   useEffect(()=>{
-    const timer = setTimeout(()=>getSearchSuggestions(),2000);
+    let timer;
+    if(searchQuery.length > 0){
+      setShow(true);
+      timer = setTimeout(()=>getSearchSuggestions().catch((e)=>console.log(e)),2000);
+    }else
+    setShow(false);
     return ()=>{
       clearTimeout(timer);
     }
@@ -31,23 +37,25 @@ const Head = () => {
           src="https://cdn1.iconfinder.com/data/icons/heroicons-ui/24/menu-512.png"
         />
         <img
-          className="h-12 w-20"
+          className="h-12 m-2 h-6 w-20"
           alt="logo"
-          src="https://cdn4.iconfinder.com/data/icons/flat-brand-logo-2/512/youtube-512.png"
+          src="https://upload.wikimedia.org/wikipedia/commons/thumb/e/e1/Logo_of_YouTube_%282015-2017%29.svg/2560px-Logo_of_YouTube_%282015-2017%29.svg.png"
         />
       </div>
       <div className="col-span-10 text-center px-10">
         <div>
         <input
+        onBlur={()=>setShow(false)}
         value = {searchQuery}
         onChange={(e)=>setSearchQuery(e.target.value)}
           className="w-1/2 border border-gray-500 p-2 rounded-l-full"
           type="text"
         />
         <button className="border border-gray-500 p-2 rounded-r-full">
-          <img className = "w-5 h-5" src="https://cdn-icons-png.flaticon.com/512/54/54481.png" alt="search"/>
+          search
         </button>
         </div>
+        { show &&
         <div className="fixed bg-white w-[37rem]">
           <ul>
             <li>  Iphone</li>
@@ -57,6 +65,7 @@ const Head = () => {
             <li>Iphone 14 pro max</li>
           </ul>
         </div>
+}
       </div>
       <div className="col-span-1">
         <img
--- src/components/WatchPage.js
+++ src/components/WatchPage.js
@@ -12,7 +12,7 @@ const WatchPage = () => {
     },[])
   return (
     <div>
-        <iframe width="800" height="500" src={"https://www.youtube.com/embed/"+ searchParams.get('v')} title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowFullScreen autoPlay></iframe>
+        <iframe width="800" height="500" src={"https://www.youtube.com/embed/"+ searchParams.get('v')+"?&autoplay=1"} title="YouTube video player" frameBorder="0" allow="accelerometer; clipboard-write;autoplay; encrypted-media; gyroscope; picture-in-picture; web-share" allowFullScreen></iframe>
     </div>
   )
 }
