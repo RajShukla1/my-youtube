c6839dd18a3cef42bedb5298e8b66942fa92a6a9
Raj <rajshukla140@gmail.com>
2024-01-09 22:48:26 +0530

    shimmer effect update

---
+++ src/components/ErrorPage.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const ErrorPage = () => {
+  return (
+    <div>
+        <h1>Sorry for the trouble</h1>
+        <h2>It will be corrected soon</h2>
+    </div>
+  )
+}
+
+export default ErrorPage
