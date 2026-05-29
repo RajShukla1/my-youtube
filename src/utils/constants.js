2137f29e5d978aa1576002665ab3eca48686a4cb
rajshukla <rajshukla140@gmail.com>
2026-05-29 15:47:15 +0530

    related videos clickable

--- src/utils/constants.js
+++ src/utils/constants.js
@@ -1,4 +1,4 @@
-export const GOOGLE_API_KEY ="AIzaSyDRxGK4y7fKmwdpGCiMLNi8fFNxCnQ3iIc";
+export const GOOGLE_API_KEY = process.env.REACT_APP_GOOGLE_API_KEY;
 export const OFFSET_LIVE_CHAT = 15;
 export const USER_IMAGE = "https://avatars.githubusercontent.com/RajShukla1";
 export const YOUTUBE_VIDEOS_API = "https://youtube.googleapis.com/youtube/v3/videos?part=snippet%2CcontentDetails%2Cstatistics&chart=mostPopular&maxResults=50&regionCode=IN&key="+GOOGLE_API_KEY;
